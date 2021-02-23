3026d95c665a8dd1eac5d2e1ec0024eff58c5300
redouane <redouane.sobaihi@live.com>
2021-02-23 19:12:56 +0700

    feat: add faculties controller

---
+++ backend/CourseBook.WebApi/Controllers/FacultiesController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseBook.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FacultiesController : ControllerBase
+    {
+    }
+}
