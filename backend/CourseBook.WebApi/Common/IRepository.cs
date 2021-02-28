aa7d5e03ac07f098796e1d40d5447f625771fd57
redouane <redouane.sobaihi@live.com>
2021-02-28 14:37:11 +0700

    feat: faculty repository (WIP)

--- backend/CourseBook.WebApi/Common/IRepository.cs
+++ backend/CourseBook.WebApi/Common/IRepository.cs
@@ -1,8 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CourseBook.WebApi.Common
 {
     internal interface IRepository<T> where T : Entity, IAggregateRoot
     {
+        Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<T>> GetAsync(CancellationToken cancellationToken = default);
     }
 }
