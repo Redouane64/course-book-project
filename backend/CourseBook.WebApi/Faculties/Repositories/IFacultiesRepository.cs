de3a56c9fb2c33a19c3d3c00e3fdf18634c8f5bf
redouane <redouane.sobaihi@live.com>
2021-02-28 14:37:11 +0700

    feat: faculty repository (WIP)

---
+++ backend/CourseBook.WebApi/Faculties/Repositories/IFacultiesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseBook.WebApi.Faculties.Repositories
+{
+    using Common;
+    using Entities;
+
+    internal interface IFacultiesRepository : IRepository<FacultyEntity>
+    {
+
+    }
+}
