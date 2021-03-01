eccd5f6d3a6407ebb5cb296ad73c9e2ad6c7af02
redouane <redouane.sobaihi@live.com>
2021-03-01 15:28:17 +0700

    feat: add fields and GroupDescipline entity and make classes sealed

--- backend/CourseBook.WebApi/Faculties/Entities/GroupEntity.cs
+++ backend/CourseBook.WebApi/Faculties/Entities/GroupEntity.cs
@@ -1,9 +1,16 @@
 namespace CourseBook.WebApi.Faculties.Entities
 {
     using System;
+    using System.Collections.Generic;
 
     using CourseBook.WebApi.Common;
 
-    internal class GroupEntity : Entity
-    { }
+    internal sealed class GroupEntity : Entity
+    {
+        public string Name { get; set; }
+
+        public DirectionEntity Direction { get; set; }
+
+        public ICollection<GroupDisciplineEntity> Disciplines { get; set; }
+    }
 }
