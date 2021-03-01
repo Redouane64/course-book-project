1e81ecf4b1269abb77d3948db0625f1fdbaf5c29
redouane <redouane.sobaihi@live.com>
2021-03-01 20:11:43 +0700

    feat: add dbcontext

---
+++ backend/CourseBook.WebApi/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿namespace CourseBook.WebApi.Data
+{
+    using Faculties.Data;
+    using Faculties.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<FacultyEntity> Faculties { get; set; }
+
+        public DbSet<DirectionEntity> Directions { get; set; }
+
+        public DbSet<DisciplineEntity> Disciplines { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+    }
+}
