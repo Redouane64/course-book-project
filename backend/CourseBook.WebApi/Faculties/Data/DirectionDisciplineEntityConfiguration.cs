94faee1e9b9df8d786adfb49042440dd70a0cc8b
redouane <redouane.sobaihi@live.com>
2021-03-01 16:45:56 +0700

    feat: add entity framework postgresql provider and entity configurations

---
+++ backend/CourseBook.WebApi/Faculties/Data/DirectionDisciplineEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace CourseBook.WebApi.Faculties.Data
+{
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class DirectionDisciplineEntityConfiguration : IEntityTypeConfiguration<DirectionDisciplineEntity>
+    {
+        public void Configure(EntityTypeBuilder<DirectionDisciplineEntity> builder)
+        {
+            builder.ToTable("directions_disciplines");
+
+            builder.HasKey(e => new {e.Direction, e.Discipline});
+
+            builder.HasOne(e => e.Direction)
+                .WithMany(e => e.Disciplines)
+                .HasForeignKey("direction_id");
+
+            builder.HasOne(e => e.Discipline)
+                .WithMany(e => e.Directions)
+                .HasForeignKey("discipline_id");
+
+        }
+    }
+}
---
+++ backend/CourseBook.WebApi/Faculties/Data/DirectionEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace CourseBook.WebApi.Faculties.Data
+{
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class DirectionEntityConfiguration : IEntityTypeConfiguration<DirectionEntity>
+    {
+        public void Configure(EntityTypeBuilder<DirectionEntity> builder)
+        {
+            builder.ToTable("directions");
+
+            builder.HasKey(e => e.Id);
+
+            builder.HasMany(e => e.Groups)
+                .WithOne(e => e.Direction)
+                .IsRequired()
+                .HasForeignKey(e => e.Direction);
+        }
+    }
+}
