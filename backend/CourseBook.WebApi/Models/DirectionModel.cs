24082f59cd9b9a05e008a824d8ce78c77251764f
致远 孙 <494726814@qq.com>
2021-03-03 15:11:25 +0700

    ..

---
+++ backend/CourseBook.WebApi/Models/DirectionModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseBook.WebApi.Models
+{
+    public class DirectionModel
+    {
+        public string Name { get; set; }
+
+        public string Faculty { get; set; }
+    }
+}
---
+++ backend/CourseBook.WebApi/Models/GroupModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseBook.WebApi.Models
+{
+    public class GroupModel
+    {
+        public string Name { get; set; }
+
+        public string Direction { get; set; }
+    }
+}
