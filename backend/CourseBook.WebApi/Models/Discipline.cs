94bfcc41c54f8f803de663e99538d3eb2a6d7a90
致远 孙 <494726814@qq.com>
2021-03-03 15:11:25 +0700

    ..

---
+++ backend/CourseBook.WebApi/Models/Discipline.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseBook.WebApi.Models
+{
+    public class Discipline
+    {
+        public string Name { get; set; }
+
+        public string Teacher { get; set; }
+
+        public string Group { get; set; }
+
+        public string Year { get; set; }
+
+        public string Description { get; set; }
+
+        public string Resourse { get; set; }
+    }
+}
