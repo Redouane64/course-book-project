94f233eca465ffc7bb3f5dd951c8f3bccab8a508
redouane <redouane.sobaihi@live.com>
2021-03-22 01:25:31 +0700

    fix: fix empty response

--- backend/CourseBook.WebApi/Controllers/FacultiesController.cs
+++ backend/CourseBook.WebApi/Controllers/FacultiesController.cs
@@ -42,7 +42,7 @@ namespace CourseBook.WebApi.Controllers
                 return NotFound();
             }
 
-            return Ok();
+            return Ok(faculty);
         }
 
         [HttpGet("{id:Guid}/directions", Name = nameof(GetFacultyDirection))]
