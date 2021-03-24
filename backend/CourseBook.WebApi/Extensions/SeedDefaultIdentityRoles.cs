f6a3d771f09f2ad47a24c6a852204cb8f746d71d
Redhouane Sobaihi <redouane.sobaihi@live.com>
2021-03-24 23:38:34 +0700

    feat: define default role names and add host extension to seed default roles through RoleManager

---
+++ backend/CourseBook.WebApi/Extensions/SeedDefaultIdentityRoles.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseBook.WebApi.Profiles.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CourseBook.WebApi.Extensions
+{
+    public static partial class HostExtensions
+    {
+        public static async Task<IHost> SeedDefaultIdentityRoles(this IHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    // seed Student role:
+                    if ((await roleManager.FindByNameAsync(Roles.StudentRoleName)) is null)
+                    {
+                        var result = await roleManager.CreateAsync(new IdentityRole(Roles.StudentRoleName));
+                        if (result.Errors.Any())
+                        {
+                            throw new Exception($"Failed to create `Student` role: {result.Errors.First().Description}");
+                        }
+
+                        logger.LogInformation("Created `Student` role successfully.");
+                    }
+
+                    // seed Teacher role:
+                    if ((await roleManager.FindByNameAsync(Roles.TeacherRoleName)) is null)
+                    {
+                        var result = await roleManager.CreateAsync(new IdentityRole(Roles.TeacherRoleName));
+                        if (result.Errors.Any())
+                        {
+                            throw new Exception($"Failed to create `Teacher` role: {result.Errors.First().Description}");
+                        }
+
+                        logger.LogInformation("Created `Teacher` role successfully.");
+                    }
+
+                    // seed Admin role:
+                    if ((await roleManager.FindByNameAsync(Roles.TeacherRoleName)) is null)
+                    {
+                        var result = await roleManager.CreateAsync(new IdentityRole(Roles.AdministratorRoleName));
+                        if (result.Errors.Any())
+                        {
+                            throw new Exception($"Failed to create `Administrator` role: {result.Errors.First().Description}");
+                        }
+
+                        logger.LogInformation("Created `Administrator` role successfully.");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while adding default identity roles to database.");
+                }
+            }
+            return webHost;
+        }
+    }
+}
--- backend/CourseBook.WebApi/Program.cs
+++ backend/CourseBook.WebApi/Program.cs
@@ -1,22 +1,23 @@
 namespace CourseBook.WebApi
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using CourseBook.WebApi.Data;
+    using CourseBook.WebApi.Extensions;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
 
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().MigrateDatabase<DataContext>().Run();
+            var host = await CreateHostBuilder(args).Build()
+                .MigrateDatabase<DataContext>()
+                .SeedDefaultIdentityRoles();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -25,27 +26,5 @@ namespace CourseBook.WebApi
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
-
-    public static class HostExtensions
-    {
-        public static IHost MigrateDatabase<T>(this IHost webHost) where T : DbContext
-        {
-            using (var scope = webHost.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var db = services.GetRequiredService<T>();
-                    db.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating the database.");
-                }
-            }
-            return webHost;
-        }
     }
 }
