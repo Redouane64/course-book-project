b0c763d4501b6c7a768c27893d33f0c249429000
致远 孙 <494726814@qq.com>
2021-04-11 19:32:24 +0700

    Add endpoint in controller

---
+++ backend/CourseBook.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+namespace CourseBook.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using CourseBook.WebApi.Admin.Queries;
+    using CourseBook.WebApi.Admin.ViewModels;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = nameof(GetUsers))]
+        [ProducesResponseType(typeof(IEnumerable<UsersViewModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetUsers(CancellationToken cancellationToken = default)
+        {
+            var users = await this._mediator.Send(new GetUsersRequest(), cancellationToken);
+            return Ok(users);
+        }
+
+        [HttpDelete("{userId:Guid}", Name = nameof(DeleteUser))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteUser([FromRoute] string id, CancellationToken cancellationToken)
+        {
+            await this._mediator.Send(new DeleteUserRequest(id), cancellationToken);
+            return NoContent();
+        }
+    }
+}
